a50fb208269506dc8c084e9a508f3bb9ab851f89
Indraneel Pole <Indraneel.Pole@daenet.com>
2015-11-03 20:48:54 +0100

    some sample app

---
+++ BlankCordovaApp1/www/app.js
@@ -0,0 +1,17 @@
+﻿Ext.application({
+    name: "BlankCordovaApp101",
+
+    launch: function () {
+        Ext.fly('appLoadingIndicator').destroy();
+
+        var button = Ext.create('Ext.Button', {
+            text: 'Test Button',
+            listeners: {
+                tap: function () {
+                    Ext.Msg.alert("Warning!", "You clicked test button");
+                }
+            }
+        });
+        Ext.Viewport.add({ xtype: 'container', padding: 10, items: [button] })
+    }
+});
